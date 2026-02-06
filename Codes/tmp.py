b4b265a5a586a9425051f82f3e4463912b672518
gdj0208 <gdj0208@gmail.com>
2026-02-06 10:08:02 +0900

    12012.py 추가

--- Codes/tmp.py
+++ Codes/tmp.py
@@ -1,28 +1,6 @@
 
-import sys
-from collections import deque
-input = sys.stdin.readline
+import math
 
-class Alg :
-    def __init__(self):
-        ...
-
-
-    def solve(self) :
-        dp = [0 for _ in range(10001)]
-        dp[1] = 1
-        dp[2] = 2
-        dp[3] = 3
-
-        for i in range(4, 10001):
-            dp[i] = dp[i-3] + int(i/2) + 1
-
-        rpt = int(input())
-        for _ in range(rpt):
-            num = int(input())
-            print(dp[num])
-
-
-alg = Alg()
-alg.solve()
-
+n1, n2 = map(int, input().split())
+ans = math.sqrt(n1 * n2)
+print(ans, ans)
