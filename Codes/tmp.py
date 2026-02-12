4eab7e64c0d8409b93542827bdc6b0e1cbb18328
gdj0208 <gdj0208@gmail.com>
2026-02-12 11:25:04 +0900

    14719.py 추가

--- Codes/tmp.py
+++ Codes/tmp.py
@@ -1,18 +1,28 @@
-import copy
 
-def ret_func(str, n, max):
-    if n > max :
-        cpy = f"{str}".replace(" ", "")
-        if eval(cpy) == 0:
-            print(f"{str}")
-        return 
-    
-    ret_func(f"{str}{' '}{n}", n+1, max)
-    ret_func(f"{str}{'+'}{n}", n+1, max)
-    ret_func(f"{str}{'-'}{n}", n+1, max)
+h, w = map(int, input().split())
 
-rpt = int(input())
-for _ in range(rpt):
-    num = int(input())
-    ret_func("1", 2, num)
-    print()
+sum = 0
+max_height = 0
+max_left = [0 for _ in range(w)]
+max_right = [0 for _ in range(w)]
+get = list(map(int, input().split()))
+ans = []
+
+m_left = 0
+m_right = 0
+for i in range(w):
+    m_left = m_left if m_left > get[i] else get[i]
+    max_left[i] = m_left
+
+    m_right = m_right if m_right > get[w-1-i] else get[w-1-i]
+    max_right[w-1-i] = m_right
+
+for i in range(w):
+    tmp = min(max_left[i], max_right[i]) - get[i]
+    ans.append(tmp)
+    sum += tmp
+
+# print("max_left : ", max_left)
+# print("max_right : ", max_right)
+# print("get : " ,get)
+print(sum)
