f792c32e9c6a7a7b5512f703117492028b9adf5b
gdj0208 <gdj0208@gmail.com>
2026-03-13 14:17:37 +0900

    2559.py 추가

--- Codes/Prefix_sum/2559.py
+++ Codes/Prefix_sum/2559.py
@@ -2,15 +2,14 @@ import sys
 N, K = map(int, input().split())
 arr = list(map(int, input().split()))
 
-sums = [0] * N
+sums = [0] * (N+1)
 sum = 0
 
 for i in range(N) :
-    sum += arr[i]
-    sums[i] = sum
+    sums[i+1] = sums[i] + arr[i]
 
-max = -sys.maxsize - 1
-for i in range(0, N-K):
+max = sums[K] - sums[0]
+for i in range(N-K+1):
     diff = sums[i+K] - sums[i]
     max = diff if max < diff else max 
 
