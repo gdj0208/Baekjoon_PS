113294f1dbd64975c977d1eea78cee1acd8bba71
gdj0208 <gdj0208@gmail.com>
2026-03-16 11:18:03 +0900

    59413.sql 추가

--- Codes/tmp.py
+++ Codes/tmp.py
@@ -1,17 +1,36 @@
-import sys
-N, K = map(int, input().split())
-arr = list(map(int, input().split()))
 
-sums = [0] * N
-sum = 0
+"""
+T에 대해 (1, T-1) ~ (T-1, 1)까지 
+- A에서 최대값, B에서 최대값을 더함
+- 이 값의 최대값을 고름
+"""
+from collections import Counter
 
-for i in range(N) :
-    sum += arr[i]
-    sums[i] = sum
+def get_sum_list(my_list, list_size) :
+    sum_list = []
+    
+    for i in range(list_size) :
+        sum = 0
+        for j in range(i, list_size):
+            sum += my_list[j]
+            sum_list.append(sum)
 
-max = -sys.maxsize - 1
-for i in range(0, N-K):
-    diff = sums[i+K] - sums[i]
-    max = diff if max < diff else max 
+    return sum_list
 
-print(max) 
+
+T = int(input())
+size_A = int(input())
+A = list(map(int, input().split()))
+size_B = int(input())
+B = list(map(int, input().split()))
+
+sum_A = get_sum_list(A, size_A)
+sum_B = get_sum_list(B, size_B) 
+
+countB = Counter(sum_B)
+cnt = 0
+for sum in sum_A :
+    target =T -sum
+    cnt += countB[target]
+
+print(cnt)
